4ba2a5426e30e1578d36ea736c886c793f2727dc
bradyJ89 <omfguess@aim.com>
2015-02-04 03:43:07 -0500

    put keys below session initialization

--- stream.py
+++ stream.py
@@ -8,14 +8,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy_declarative import Tweet, User, Price
 from textblob import TextBlob
 
-# check your credentials
-print("check your credentials")
-
-consumer_key = "rY3Q4lLIAcLRXPm66JoU2jL8X"
-consumer_secret = "xkTrpkamaiDQaiAdEvcvJLj6hmaLH0DL2m5bE4l4H7ROFuRKBC"
-access_token = "928665026-VghhFE4Xxovwv1Sz7Ivizdm6bGjEQn2yFGgd5TIy"
-access_token_secret = "xtdeTR1eEkSwlhPwj02OLle64kPFvBUYgfx9FsuaozZdI"
-
 username = "postgres"
 password = "password"
 port = "5433"
@@ -30,6 +22,11 @@ print ( "Connected!\n" )
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+consumer_key = "rY3Q4lLIAcLRXPm66JoU2jL8X"
+consumer_secret = "xkTrpkamaiDQaiAdEvcvJLj6hmaLH0DL2m5bE4l4H7ROFuRKBC"
+access_token = "928665026-VghhFE4Xxovwv1Sz7Ivizdm6bGjEQn2yFGgd5TIy"
+access_token_secret = "xtdeTR1eEkSwlhPwj02OLle64kPFvBUYgfx9FsuaozZdI"
 
 count = 0
 positive_count = 0
