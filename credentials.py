f3b6d26aa9d5eaf98012b230bd02033c67c12319
jbrady89 <omfguess@aim.com>
2015-09-10 23:00:34 -0400

    remove

--- credentials.py
+++
@@ -1,4 +0,0 @@
-key = "irbICd8MdaQ0r2o6zvrfvwfdz"
-secret = "TkocspkbAASks3vYv896pwh3bBEmxihzKwoT4ZtQblyZ4UhsoE"
-token = "3044811100-1G3VrzIsOEkm1cTAXa15wqBH0O56hfwDmQCVcWF"
-token_secret = "fDhL7RsFINKV3qg5hym1KDs0a1SGLW0R6i17aAL28nj9V"
