a50553352a68f5007c30cd7b8ad6a709d86581a7
jbrady89 <omfguess@aim.com>
2015-10-28 18:45:47 -0400

    cleanup

--- trump.py
+++ trump.py
@@ -1,18 +1,18 @@
 import datetime, threading, re, time, json, requests, langid, sys, traceback
-import tweepy, pymongo, config
+import tweepy, pymongo
+import credentials as config
 from pymongo import MongoClient
-from geopy.geocoders import Nominatim
+#from geopy.geocoders import Nominatim
 from tweepy import Stream, OAuthHandler, StreamListener
-import twitter
-from sqlalchemy import create_engine, Column, Integer, Float, Text, Boolean
-from sqlalchemy import DateTime
-from sqlalchemy import or_
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy_declarative import Tweet, User, Price
+#import twitter
+# from sqlalchemy import create_engine, Column, Integer, Float, Text, Boolean
+# from sqlalchemy import DateTime
+# from sqlalchemy import or_
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy_declarative import Tweet, User, Price
 from textblob import TextBlob
 
-print(config)
 consumer_key = config.key
 consumer_secret = config.secret 
 access_token = config.token
@@ -35,21 +35,10 @@ db = client.geostream
 print(db)
 posts = db.posts
 tweets = db.tweets
-geolocator = Nominatim()
 
-
-#print ( "Connecting to database\n")
-
-#engine = create_engine("postgresql+psycopg2://{}:{}@localhost:{}/{}".format(username, password, port, db))
-
-#print ( "Connected!\n" )
-
-#Base = declarative_base()
-#Session = sessionmaker(bind=engine)
 global session
 global auth
 global api
-#session = Session()
 
 count = 0
 positive_count = 0
@@ -148,31 +137,7 @@ def get_sentiment(created_at, tweet_id, username, user_id, favorited, favorite_c
         #print(formatted_date)
         timestamp = created_at
 
-        
-    #user = session.query(User).filter(or_(User.user_id==user_id, User.username==username)).first()
-    #tweet = session.query(Tweet).filter_by(tweet_id=tweet_id).first()
     user = posts.find({'user.screen_name' : username}).count()
-
-    # if user:
-    #     print 'user exists'
-
-    #     time.sleep(5)
-    #     timestamp = time.time() * 1000
-    #     tweet = {
-    #         'tweet_id' : tweet_id,
-    #         'user_id' : user_id,
-    #         'username' : username,
-    #         'created_at' : created_at,
-    #         'timestamp' : timestamp,
-    #         'sentiment' : polarity,
-    #         'average_sentiment' : polarity_average,
-    #         'text' : text
-    #     }
-
-    #     posts.insert(tweet)
-
-    # else:
-    #     print('adding new user')
         
     timestamp = time.time() * 1000
     tweet = {
@@ -186,9 +151,9 @@ def get_sentiment(created_at, tweet_id, username, user_id, favorited, favorite_c
         'text' : text
     }
 
-    if coordinates:
-    	print(coordinates)
-    	tweet.coordinates = coordinates
+    # if coordinates:
+    # 	print(coordinates)
+    # 	tweet.coordinates = coordinates
 
 
     try:
@@ -199,10 +164,7 @@ def get_sentiment(created_at, tweet_id, username, user_id, favorited, favorite_c
 
 def process_data(data):
     tweet = json.loads(data)
-    #print(tweet)
-    #posts.insert(tweet)
-    #output formatted json to the console
-    #print( json.dumps( tweet, sort_keys=True, indent=4, separators=(',', ': ') ) )
+
     if 'RT' not in tweet['text'].upper(): 
         #print(tweet['coordinates'])
         tweet_id = tweet['id']
@@ -210,7 +172,6 @@ def process_data(data):
         followers = tweet['user']['followers_count']
         following = tweet['user']['friends_count']
         location = tweet['user']['location']
-        print(geolocator.geocode(location))
         retweeted = tweet['retweeted']
         retweet_count = tweet['retweet_count']
         favorited = tweet['favorited']
@@ -226,13 +187,15 @@ def process_data(data):
         # 	#coordinates = place_data.centroid
 
         text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
-        #print("text: {}".format(text))
+        print("text: {}".format(text))
+
         matches = tweets.find({'text' : text}).count()
         #print(matches)
+
         if matches:
             pass
         else:
-            #print("no matches")
+            print("no matches")
             get_sentiment(created_at, tweet_id, username, user_id, favorited, favorite_count, retweeted, retweet_count, followers, following, coordinates=None)
 
 
